e6e0e9fd6c5fa63d3ec49466a0f9ea0762c85ad4
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-09-12 17:15:37 -0700

    Add pi sound effect

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ window.addEventListener("load", function() {
 
     //audio stuff
     document.getElementById("settingsAudio").value = window.localStorage.getItem("piwebosAudio");
+    if(document.getElementById("settingsAudio").value == "") {
+        document.getElementById("settingsAudio").value = "on";
+    }
+    document.getElementById("settingsPiAudio").value = window.localStorage.getItem("piwebosPiAudio");
+    if(document.getElementById("settingsPiAudio").value == "") {
+        document.getElementById("settingsPiAudio").value = "snap";
+    }
+    globalThis.thisDigit = -1;
 
     //time stuff
     resetTime();
     document.getElementById("settingsTime").value = window.localStorage.getItem("piwebosTime");
+    if(document.getElementById("settingsTime").value == "") {
+        document.getElementById("settingsTime").value = "sanity";
+    }
 
     //date stuff
     resetDate();
@@ -259,11 +270,33 @@ function settingsAudio(thisElement) {
     window.localStorage.setItem("piwebosAudio", thisElement.value);
 }
 
+function settingsPiAudio(thisElement) {
+    window.localStorage.setItem("piwebosPiAudio", thisElement.value);
+}
+
 function clickAudio() {
+    const toPlay = window.localStorage.getItem("piwebosPiAudio");
+    if(toPlay == "pi") {
+        piAudio();
+        return;
+    }
     const preferences = window.localStorage.getItem("piwebosAudio");
     if(preferences == "off") {
         return;
     }
-    const effect = new Audio("snap.wav");
+    const effect = new Audio("audio/snap.wav");
     effect.play();
+}
+
+function piAudio() {
+    const digits = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989";
+    globalThis.thisDigit = thisDigit + 1;
+    const digitPlay = digits[thisDigit];
+    if(digitPlay == ".") {
+        const toPlay = new Audio("audio/point.wav");
+        toPlay.play();
+    } else {
+        const toPlay = new Audio(`audio/${digitPlay}.wav`);
+        toPlay.play();
+    }
 }
